d9e301c35c4e08e47e3bd52a8f77bda38e838119
EMD_HB <hitoli03@163.com>
2019-07-07 14:10:27 +0800

    add comment changed

--- src/main/java/com/hb/monitor/MonitorApplication.java
+++ src/main/java/com/hb/monitor/MonitorApplication.java
@@ -3,6 +3,9 @@ package com.hb.monitor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Do Change Something
+ */
 @SpringBootApplication
 public class MonitorApplication {
 
